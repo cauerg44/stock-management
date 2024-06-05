6882ba28659dbeb6dc35f04a4b5c786f9dcda792
Caue Garcia <cauerg7@outlook.com>
2024-06-05 09:58:55 -0300

    plugin maven, created entities - product and category.

---
+++ back-end/spring/src/main/java/com/appfullstack/backend/entities/Product.java
@@ -0,0 +1,100 @@
+package com.appfullstack.backend.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import com.appfullstack.backend.enums.Rating;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "procuts")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	private Rating rating;
+	private String description;
+	
+	private Set<Category> categories = new HashSet<>();
+	
+	public Product() {
+	}
+
+	public Product(Long id, String name, Double price, Rating rating, String description) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.rating = rating;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Rating getRating() {
+		return rating;
+	}
+
+	public void setRating(Rating rating) {
+		this.rating = rating;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ back-end/spring/src/main/java/com/appfullstack/backend/enums/Rating.java
@@ -0,0 +1,9 @@
+package com.appfullstack.backend.enums;
+
+public enum Rating {
+
+	MEDIUM,
+	GOOD,
+	EXCELENT;
+	
+}
