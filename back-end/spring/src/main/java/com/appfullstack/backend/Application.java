237100b16834533d6c3defdf876fbeb5205ede6d
Caue Garcia <cauerg7@outlook.com>
2024-06-05 09:05:21 -0300

    renaming sub-packages.

--- back-end/spring/src/main/java/com/appfullstack/backend/Application.java
+++ back-end/spring/src/main/java/com/appfullstack/backend/Application.java
@@ -1,4 +1,4 @@
-package com.appfullstack.spring;
+package com.appfullstack.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back-end/spring/src/test/java/com/appfullstack/backend/ApplicationTests.java
+++ back-end/spring/src/test/java/com/appfullstack/backend/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.appfullstack.spring;
+package com.appfullstack.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
