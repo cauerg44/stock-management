ef73b313be435ce4165de195054273fa1cbf2d84
Caue Garcia <cauerg7@outlook.com>
2024-06-06 17:27:04 -0300

    post products, but ocourring erros: null values.

--- back-end/spring/src/main/java/com/appfullstack/backend/controllers/ProductController.java
+++ back-end/spring/src/main/java/com/appfullstack/backend/controllers/ProductController.java
@@ -1,13 +1,18 @@
 package com.appfullstack.backend.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.appfullstack.backend.dto.ProductDTO;
 import com.appfullstack.backend.services.ProductService;
@@ -29,5 +34,13 @@ public class ProductController {
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
 	}
 }
--- back-end/spring/src/main/java/com/appfullstack/backend/services/ProductService.java
+++ back-end/spring/src/main/java/com/appfullstack/backend/services/ProductService.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.appfullstack.backend.dto.CategoryDTO;
 import com.appfullstack.backend.dto.ProductDTO;
+import com.appfullstack.backend.entities.Category;
 import com.appfullstack.backend.entities.Product;
+import com.appfullstack.backend.entities.Supplier;
 import com.appfullstack.backend.repositories.ProductRepository;
 import com.appfullstack.backend.services.exceptions.ResourceNotFoundException;
 
@@ -28,5 +31,33 @@ public class ProductService {
 		Product prod = repository.findById(id).orElseThrow(
 				() -> new ResourceNotFoundException("Resource not found."));
 		return new ProductDTO(prod);
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		dtoToEntity(entity, dto);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void dtoToEntity(Product entity, ProductDTO dto) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setManufactureDate(dto.getManufactureDate());
+		entity.setRating(dto.getRating());
+		
+		Supplier supplier = new Supplier();
+		if (dto.getSupplier() != null) {
+		    supplier.setId(dto.getSupplier().getId());
+		}
+		entity.setSupplier(supplier);
+		
+		entity.getCategories().clear();
+		for (CategoryDTO categoriesDTO : dto.getCategories()) {
+			Category category = new Category();
+			category.setId(categoriesDTO.getId());
+			entity.getCategories().add(category);
+		}
 	}
 }
