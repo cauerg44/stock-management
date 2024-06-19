3dce36918cf02bc061202fd276f35664ca84cbef
Caue Garcia <cauerg7@outlook.com>
2024-06-19 14:45:49 -0300

    insert product test ok.

--- back-end/spring/src/test/java/com/appfullstack/backend/services/ProductServiceTests.java
+++ back-end/spring/src/test/java/com/appfullstack/backend/services/ProductServiceTests.java
@@ -19,22 +19,34 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.appfullstack.backend.dto.ProductDTO;
+import com.appfullstack.backend.entities.Category;
 import com.appfullstack.backend.entities.Product;
+import com.appfullstack.backend.entities.Supplier;
+import com.appfullstack.backend.repositories.CategoryRepository;
 import com.appfullstack.backend.repositories.ProductRepository;
+import com.appfullstack.backend.repositories.SupplierRepository;
 import com.appfullstack.backend.services.exceptions.ResourceNotFoundException;
 import com.appfullstack.backend.tests.ProductFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
-	
+
 	@InjectMocks
 	private ProductService service;
 
 	@Mock
 	private ProductRepository repository;
-	
+
+	@Mock
+	private SupplierRepository supplierRepository;
+
+	@Mock
+	private CategoryRepository categoryRepository;
+
 	private long existingProductId, nonExistingProductId, dependentProductId;
 	private String productName;
+	private Category category;
+	private Supplier supplier;
 	private Product product;
 	private ProductDTO productDTO;
 	private PageImpl<Product> page;
@@ -44,37 +56,42 @@ public class ProductServiceTests {
 		existingProductId = 1L;
 		nonExistingProductId = 2L;
 		dependentProductId = 3L;
-		
+
 		productName = "Coffe cup";
-		
+
 		product = ProductFactory.createProduct(productName);
 		productDTO = new ProductDTO(product);
+		supplier = product.getSupplier();
+		category = product.getCategories().iterator().next();
+
 		page = new PageImpl<>(List.of(product));
-		
+
 		Mockito.when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
 		Mockito.when(repository.findById(nonExistingProductId)).thenReturn(Optional.empty());
 		
-		Mockito.when(repository.findAll((Pageable)any())).thenReturn(page);
+		Mockito.when(repository.findAll((Pageable) any())).thenReturn(page);
+		
+		Mockito.when(repository.save(any())).thenReturn(product);
+		
+		Mockito.when(supplierRepository.findById(supplier.getId())).thenReturn(Optional.of(supplier));
+		Mockito.when(categoryRepository.findById(category.getId())).thenReturn(Optional.of(category));
 	}
-	
+
 	@Test
 	public void findByIdShouldReturnProductDTO() {
-		
 		ProductDTO dto = service.findById(existingProductId);
-		
 		Assertions.assertNotNull(dto);
 		Assertions.assertEquals(dto.getId(), existingProductId);
 		Assertions.assertEquals(dto.getName(), product.getName());
 	}
-	
+
 	@Test
 	public void findByIdShouldReturnResourceNotFoundExceptionWhenIdDoesNotExists() {
-		
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.findById(nonExistingProductId);
 		});
 	}
-	
+
 	@Test
 	public void findAllShouldReturnPagedProductDTO() {
 		
@@ -85,5 +102,14 @@ public class ProductServiceTests {
 		Assertions.assertNotNull(result);
 		Assertions.assertEquals(result.getSize(), 1);
 		Assertions.assertEquals(result.iterator().next().getName(), productName);
+	}
+
+	@Test
+	public void insertShouldReturnProductDTO() {
+		
+		ProductDTO result = service.insert(productDTO);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), product.getId());
 	}
 }
