8ca8df6ed4ad805cdf0f6f8c289c21f8a1d26bb1
Caue Garcia <cauerg7@outlook.com>
2024-06-28 15:57:33 -0300

    UserControllerIT done.

--- back-end/spring/src/test/java/com/appfullstack/backend/controllers/UserControllerIT.java
+++ back-end/spring/src/test/java/com/appfullstack/backend/controllers/UserControllerIT.java
@@ -1,10 +1,17 @@
 package com.appfullstack.backend.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.appfullstack.backend.tests.TokenUtil;
 
@@ -32,5 +39,48 @@ public class UserControllerIT {
 		clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
 		stockManagerToken = tokenUtil.obtainAccessToken(mockMvc, stockManagerUsername, stockManagerPassword);
 		invalidToken = stockManagerPassword + "invalid"; // Simulating an invalid token
+	}
+	
+	@Test
+	public void getMeShouldReturnUserDTOWhenStockManagerLogged() throws Exception {
+		
+		ResultActions result = 
+				mockMvc.perform(get("/users/me")
+					.header("Authorization", "Bearer " + stockManagerToken)
+					.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(1L));
+		result.andExpect(jsonPath("$.name").value("Cauê Garcia"));
+		result.andExpect(jsonPath("$.email").value("caue@outlook.com"));
+		result.andExpect(jsonPath("$.password").value("$2a$10$QrDGMknxOAoy6zVSV23RaOS94rsIj/6VpG9drqHGrI9.AIkNysCh2"));
+		result.andExpect(jsonPath("$.roles").exists());
+	}
+	
+	@Test
+	public void getMeShouldReturnUserDTOWhenClientLogged() throws Exception {
+		
+		ResultActions result = 
+				mockMvc.perform(get("/users/me")
+					.header("Authorization", "Bearer " + clientToken)
+					.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(3L));
+		result.andExpect(jsonPath("$.name").value("Junior Ciqueira"));
+		result.andExpect(jsonPath("$.email").value("junior@gmail.com"));
+		result.andExpect(jsonPath("$.password").value("$2a$10$QrDGMknxOAoy6zVSV23RaOS94rsIj/6VpG9drqHGrI9.AIkNysCh2"));
+		result.andExpect(jsonPath("$.roles").exists());
+	}
+	
+	@Test
+	public void getMeShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+		
+		ResultActions result = 
+				mockMvc.perform(get("/users/me")
+					.header("Authorization", "Bearer " + invalidToken)
+					.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isUnauthorized());
 	}
 }
