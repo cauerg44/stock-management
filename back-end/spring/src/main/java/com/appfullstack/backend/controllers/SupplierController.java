d5b15d8cc866a81ed316759842dffedbceffc4f6
Caue Garcia <cauerg7@outlook.com>
2024-06-29 10:51:04 -0300

    SupplierController and UserController documented with Swagger.

--- back-end/spring/src/main/java/com/appfullstack/backend/controllers/SupplierController.java
+++ back-end/spring/src/main/java/com/appfullstack/backend/controllers/SupplierController.java
@@ -19,6 +19,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.appfullstack.backend.dto.SupplierDTO;
 import com.appfullstack.backend.services.SupplierService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -30,22 +32,50 @@ public class SupplierController {
 	@Autowired
 	private SupplierService service;
 	
+	@Operation(
+		    description = "Get all suppliers",
+		    summary = "List all suppliers",
+		    responses = {
+		         @ApiResponse(description = "Ok", responseCode = "200"),
+		         @ApiResponse(description = "Unauthorized", responseCode = "401")
+		    }
+		)
 	@PreAuthorize("hasAnyRole('ROLE_STOCK_MANAGER', 'ROLE_CLIENT')")
-	@GetMapping
+	@GetMapping(produces =  "application/json")
 	public ResponseEntity<List<SupplierDTO>> findAll(){
 		List<SupplierDTO> suppliers = service.findAll();
 		return ResponseEntity.ok(suppliers);
 	}
 	
+	@Operation(
+		    description = "Get supplier by id",
+		    summary = "Get supplier by id",
+		    responses = {
+		         @ApiResponse(description = "Ok", responseCode = "200"),
+		         @ApiResponse(description = "Not found", responseCode = "404"),
+		         @ApiResponse(description = "Unauthorized", responseCode = "401")
+		    }
+		)
 	@PreAuthorize("hasAnyRole('ROLE_STOCK_MANAGER', 'ROLE_CLIENT')")
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}", produces =  "application/json")
 	public ResponseEntity<SupplierDTO> findById(@PathVariable Long id) {
 		SupplierDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
 	}
 	
+	@Operation(
+		    description = "Create a new supplier",
+		    summary = "Create a new supplier",
+		    responses = {
+		         @ApiResponse(description = "Created", responseCode = "201"),
+		         @ApiResponse(description = "Bad Request", responseCode = "400"),
+		         @ApiResponse(description = "Unauthorized", responseCode = "401"),
+		         @ApiResponse(description = "Forbidden", responseCode = "403"),
+		         @ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+		    }
+		)
 	@PreAuthorize("hasRole('ROLE_STOCK_MANAGER')")
-	@PostMapping
+	@PostMapping(produces =  "application/json")
 	public ResponseEntity<SupplierDTO> insert(@Valid @RequestBody SupplierDTO dto) {
 		dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -53,15 +83,37 @@ public class SupplierController {
         return ResponseEntity.created(uri).body(dto);
 	}
 	
+	@Operation(
+		    description = "Update a supplier",
+		    summary = "Update a supplier",
+		    responses = {
+		         @ApiResponse(description = "Ok", responseCode = "200"),
+		         @ApiResponse(description = "Not found", responseCode = "404"),
+		         @ApiResponse(description = "Unauthorized", responseCode = "401"),
+		         @ApiResponse(description = "Forbidden", responseCode = "403"),
+		         @ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+		    }
+		)
 	@PreAuthorize("hasRole('ROLE_STOCK_MANAGER')")
-	@PutMapping(value = "/{id}")
+	@PutMapping(value = "/{id}", produces =  "application/json")
 	public ResponseEntity<SupplierDTO> update(@PathVariable Long id, @Valid @RequestBody SupplierDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
 	
+	@Operation(
+		    description = "Delete a supplier",
+		    summary = "Delete a suppliergit s",
+		    responses = {
+		         @ApiResponse(description = "Sucess", responseCode = "204"),
+		         @ApiResponse(description = "Bad request", responseCode = "400"),
+		         @ApiResponse(description = "Unauthorized", responseCode = "401"),
+		         @ApiResponse(description = "Forbidden", responseCode = "403"),
+		         @ApiResponse(description = "Not found", responseCode = "404"),
+		    }
+		)
 	@PreAuthorize("hasRole('ROLE_STOCK_MANAGER')")
-	@DeleteMapping(value = "/{id}")
+	@DeleteMapping(value = "/{id}", produces =  "application/json")
 	public ResponseEntity<Void> delete(@Valid @PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
--- back-end/spring/src/main/java/com/appfullstack/backend/controllers/UserController.java
+++ back-end/spring/src/main/java/com/appfullstack/backend/controllers/UserController.java
@@ -10,18 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.appfullstack.backend.dto.UserDTO;
 import com.appfullstack.backend.services.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(value = "/users")
-@Tag(name = "Suppliers", description = "Controller for Supplier")
+@Tag(name = "Users", description = "Controller for User")
 public class UserController {
 
 	@Autowired
 	private UserService service;
 	
+	@Operation(
+		    description = "Get user logged",
+		    summary = "Get user logged",
+		    responses = {
+		         @ApiResponse(description = "Ok", responseCode = "200"),
+		         @ApiResponse(description = "Unauthorized", responseCode = "401")
+		    }
+		)
 	@PreAuthorize("hasAnyRole('ROLE_STOCK_MANAGER', 'ROLE_CLIENT')")
-	@GetMapping(value = "/me")
+	@GetMapping(value = "/me", produces =  "application/json")
 	public ResponseEntity<UserDTO> getMyself() {
 		UserDTO dto = service.getMyself();
 		return ResponseEntity.ok(dto);
