f0c1ea29dacdadc6bf995e9d315fb6d9478accec
Caue Garcia <cauerg7@outlook.com>
2024-06-29 09:53:21 -0300

    Config class OpenAPI.

---
+++ back-end/spring/src/main/java/com/appfullstack/backend/config/OpenAPIConfig.java
@@ -0,0 +1,26 @@
+package com.appfullstack.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+
+	@Bean
+    public OpenAPI stockControllerAPI() {
+        return new OpenAPI()
+           .info(new Info()
+           .title("Stock Controller API")
+           .description("Product inventory management")
+           .version("v0.0.1")
+           .license(new License()
+           .name("Apache 2.0")
+           .url("https://github.com/cauerg44/product-stock")));
+    }
+}
